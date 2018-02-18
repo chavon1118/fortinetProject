2e38ac331b38f488c4d9799f4ea4f98bce28a1c9
chavon1118 <chavonzhou@gmail.com>
2018-02-17 21:55:07 -0800

    [upload feature] basic upload json file feature
    - added code to upload and parse json to create Threat object (TODO: improve code by modify code to not have to read json field one by one)

--- threatProject/threatapp/views.py
+++ threatProject/threatapp/views.py
@@ -1,5 +1,9 @@
-from django.http import HttpResponse
+import json
+from datetime import datetime
+from django.http import HttpResponseRedirect
 from django.views import generic
+from django.shortcuts import render
+from django.urls import reverse
 from .models import Threat
 
 class IndexView(generic.ListView):
@@ -11,4 +15,14 @@ class IndexView(generic.ListView):
         return Threat.objects.order_by('date')
 
 def uploadfile(request):
-    return HttpResponse("TODO: change this to upload json file")
+    if request.method == 'POST':
+        threatfile = request.FILES['threatfile']
+        for chunk in threatfile.chunks():
+            print("chunk: %s" %chunk)
+            data = json.loads(chunk.decode('utf-8').replace("'",'"'))
+            print("data: %s" %data)
+            for threat in data:
+                Threat.objects.update_or_create(date=datetime.strptime(threat['date'],'%b %d, %Y %H:%M:%S'), filename=threat['filename'],
+                action=threat['action'],submit_type=threat['submit-type'],rating=threat['rating'])
+            return HttpResponseRedirect(reverse('threatapp:index'))
+    return render(request,'threatapp/index.html',{'form':form})
