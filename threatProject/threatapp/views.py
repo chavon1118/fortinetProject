f00ac4ea88b9dfe20bc7665b2183428fff091998
chavon1118 <chavonzhou@gmail.com>
2018-02-18 17:22:16 -0800

    [save to dir] add a media dir to allow saving uploaded meta files

--- threatProject/threatapp/views.py
+++ threatProject/threatapp/views.py
@@ -1,10 +1,11 @@
 import json
 from datetime import datetime
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
 from django.views import generic
 from django.shortcuts import render
 from django.urls import reverse
 from .models import Threat
+from django.core.files.storage import FileSystemStorage
 
 class IndexView(generic.ListView):
     template_name = 'threatapp/index.html'
@@ -14,9 +15,15 @@ class IndexView(generic.ListView):
     def get_queryset(self):
         return Threat.objects.order_by('date')
 
+# user can upload json meta file to add threats to database
 def uploadfile(request):
-    if request.method == 'POST':
+    if request.method == 'POST' and request.FILES['threatfile']:
         threatfile = request.FILES['threatfile']
+        # save file to media dir
+        fs = FileSystemStorage()
+        filename = fs.save(threatfile.name, threatfile)
+        uploaded_file_url = fs.url(filename)
+        response = {'uploaded_file_url': uploaded_file_url}
         for chunk in threatfile.chunks():
             print("chunk: %s" %chunk)
             data = json.loads(chunk.decode('utf-8').replace("'",'"'))
@@ -24,4 +31,5 @@ def uploadfile(request):
             for threat in data:
                 Threat.objects.update_or_create(date=datetime.strptime(threat['date'],'%b %d, %Y %H:%M:%S'), filename=threat['filename'],
                 action=threat['action'],submit_type=threat['submit-type'],rating=threat['rating'])
+        return JsonResponse(response)
     return HttpResponseRedirect(reverse('threatapp:index'))
